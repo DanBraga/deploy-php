1f840b1a96572aca55a99a7d9e54224fae42401f
DANIEL DA SILVA BRAGA <987288272@itaud.des.ihf>
2021-02-02 14:39:43 -0300

    atualizando dados do site v2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<h1 style="color: red" Efetuando teste para a nova aplicacao - Dani Braga </h1>
+<h1 style="color: red"> Teste site Dani Braga</h1>
 
 
 <div style="display:flex;height: 400px;width:100%;color: #8c8c8c;font-size: 30px"> 
