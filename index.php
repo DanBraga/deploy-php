652ef4868ce60c26eaeb30e174b5720bb42aefb2
DANIEL DA SILVA BRAGA <987288272@itaud.des.ihf>
2021-02-02 14:05:39 -0300

    atualizando dados do site v2

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 <h1 style="color: red" Efetuando teste para a nova aplicacao </h1>
-  
+
+novo timezone_offset_get
+
 <div style="display>flex">
     <div style="background-color: blue">
     Coluna cor azul
